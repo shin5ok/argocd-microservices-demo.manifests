0e3a0ac10061e20ba5efb922b8f6c44fa600efc3
Shingo <shingo.google@55mp.com>
2020-07-28 15:07:09 +0900

    to implement to apps/v1 in 1.16

---
+++ t.py
@@ -0,0 +1,19 @@
+import json
+import glob
+import yaml
+
+# def getorg():
+   
+
+def main():
+    for dep in glob.glob("*-dep.yaml"):
+        try:
+            with open(dep) as f:
+                j = yaml.safe_load(f)
+                j['spec']['selector'] = {"matchLabels":{"name":j["metadata"]["name"]}}
+                with open(f"new-{dep}", "w") as fw:
+                    fw.write(yaml.dump(j))
+        except Exception as e:
+            print(str(e))
+
+main()
